00771d5cadd29ef1e78c3e444c3071df0f9e2a7b
yoshinkun2210 <yoshimkun22@gmail.com>
2022-10-19 11:08:07 +0700

    th

---
+++ src/main/java/rikkei/academy/controller/UserController.java
@@ -0,0 +1,34 @@
+package rikkei.academy.controller;
+
+import model.Login;
+import model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import service.UserService;
+
+@Controller
+public class UserController {
+    @GetMapping(value = {"/", "/home"})
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home", "login", new Login());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login) {
+        User user = UserService.checkLogin(login);
+        if (user == null) {
+            ModelAndView modelAndView = new ModelAndView("error");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 19/10/2022
+  Time: 11:00 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<h3>Account: ${user.account}</h3>
+<h3>Name: ${user.name}</h3>
+<h3>Email: ${user.email}</h3>
+<h3>Age: ${user.age}</h3>
+
+</body>
+</html>
